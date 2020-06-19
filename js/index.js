a046634ec6e5c697c9550740932842d07c884664
Eddy Ndrantonavalona <eddy@madagascar-internet.com>
2020-06-19 13:23:33 +0300

    Ajout du combo de la date de naissance

--- js/index.js
+++ js/index.js
@@ -1,23 +1,35 @@
 $(document).on('ready', function () {
     var date = new Date(),
-        lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0),
+        lastDay = date.getDate(),
+        currentMonth = date.getMonth() + 1,
+        currentMonth = (currentMonth < 10) ? '0'+currentMonth : currentMonth,
         currentYear = date.getFullYear(),
-        year_required = currentYear - 18;
+        year_required = currentYear - 18,    
+        lastday_in_month = new Date(currentYear, currentMonth, 0).getDate();
+
     function loop_date(el,start, end) {
         while (start <= end) {
             if (start < 10) {
                 start = '0'+start;
             }
             $('#'+el).append("<option value="+start+">"+start+"</option>");
+            if ($('#'+el).val() == start) {
+                
+            }
             start++;
         }
     }
     // Boucle date
-    // loop_date('jour_naissance', 1, lastDay);
-    // // Boucle mois
-    // loop_date('mois_naissance', 1, 12);
-    // // Boucle annee
-    // loop_date('annee_naissance', 1940, year_required);
+    loop_date('jour_naissance', 1, lastday_in_month);
+    // Boucle mois
+    loop_date('mois_naissance', 1, 12);
+    // Boucle annee
+    loop_date('annee_naissance', 1940, year_required);
+    // $('#jour_naissance').val()
+    if ($('#jour_naissance').val() == lastDay) {
+        $('#jour_naissance').val(lastDay).prop('selected', true);
+    }
+    console.log($('#jour_naissance').val());
     $(document).on('click', '[id^=login_]', function (e) {
         e.preventDefault();
         $("#modalLogin").modal();

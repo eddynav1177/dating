cca78c0fe6b0503fdf487eb8d848d645534c137b
Eddy Ndrantonavalona <eddy@madagascar-internet.com>
2020-06-25 13:05:56 +0300

    Modification de l'apparition du cgv

--- js/payment.js
+++ js/payment.js
@@ -1,51 +1,37 @@
 $(document).ready(function() {
     sessionStorage.clear();
+    
+    function controleInput(el, interval, delimiter) {
+        var el_id = document.getElementById(el);
+        el_id.onkeyup = function (e) {
+            if (this.value == this.lastValue) return;
+            var caretPosition = this.selectionStart;
+            var sanitizedValue = this.value.replace(/[^0-9]/gi, '');
+            var parts = [];
+
+            // for (var i = 0, len = sanitizedValue.length; i < len; i +=interval) { 
+                for (var i = 0; i < sanitizedValue.length; i +=interval) {
+                parts.push(sanitizedValue.substring(i, i + interval));
+            }
+
+            for (var i=caretPosition - 1; i>= 0; i--) {
+                var c = this.value[i];
+                if (c < '0' || c> '9') {
+                    caretPosition--;
+                }
+            }
+            caretPosition += Math.floor(caretPosition / interval);
+            
+            this.value = this.lastValue = parts.join(delimiter);
+            this.selectionStart = this.selectionEnd = caretPosition;
+        }
+    }
+
     //For Card Number formatted input
-    var cardNum = document.getElementById('cr_no');
-    cardNum.onkeyup = function (e) {
-        if (this.value == this.lastValue) return;
-        var caretPosition = this.selectionStart;
-        var sanitizedValue = this.value.replace(/[^0-9]/gi, '');
-        var parts = [];
-        
-        for (var i = 0, len = sanitizedValue.length; i < len; i +=4) { 
-            parts.push(sanitizedValue.substring(i, i + 4)); 
-        } 
-        for (var i=caretPosition - 1; i>= 0; i--) {
-            var c = this.value[i];
-            if (c < '0' || c> '9') {
-                caretPosition--;
-            }
-        }
-        caretPosition += Math.floor(caretPosition / 4);
-        
-        this.value = this.lastValue = parts.join(' ');
-        this.selectionStart = this.selectionEnd = caretPosition;
-    }
-    
+    controleInput('cr_no', 4, ' ');
     //For Date formatted input
-    var expDate = document.getElementById('exp');
-    expDate.onkeyup = function (e) {
-        if (this.value == this.lastValue) return;
-        var caretPosition = this.selectionStart;
-        var sanitizedValue = this.value.replace(/[^0-9]/gi, '');
-        var parts = [];
-    
-        for (var i = 0, len = sanitizedValue.length; i < len; i +=2) { 
-            parts.push(sanitizedValue.substring(i, i + 2)); 
-        } 
-        for (var i=caretPosition - 1; i>= 0; i--) {
-            var c = this.value[i];
-            if (c < '0' || c> '9') {
-                caretPosition--;
-            }
-        }
-        caretPosition += Math.floor(caretPosition / 2);
-    
-        this.value = this.lastValue = parts.join('/');
-        this.selectionStart = this.selectionEnd = caretPosition;
-    }
-    
+    controleInput('exp', 2, '/');
+
     $(window).on('scroll', function(e) {
         var scroll_top = $(this).scrollTop();
         if (scroll_top >= 0 && scroll_top <= 90) {
@@ -54,11 +40,5 @@ $(document).ready(function() {
             $("#section_cgv").removeClass('hide');
         }
     });
-
-    $(document).on('click', '#suite_cgv', function () {
-        /*if ($('#plus_cgv').hasClass('hide')) {
-
-        }*/
-        $('#plus_cgv').slideToggle('hide');
-    });
+    
 });
